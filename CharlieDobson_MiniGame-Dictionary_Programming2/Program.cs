e032f428119f8d40c81fb432e933c01896c76551
C-DGameMaker <charlotte5634@outlook.com>
2026-01-30 11:28:03 -0400

    Able to add a new monster

--- CharlieDobson_MiniGame-Dictionary_Programming2/Program.cs
+++ CharlieDobson_MiniGame-Dictionary_Programming2/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CharlieDobson_MiniGame_Dictionary_Programming2
 {
@@ -11,14 +8,9 @@ namespace CharlieDobson_MiniGame_Dictionary_Programming2
         static void Main(string[] args)
         {
             /*
-             Initialize a dictionary called monsterHealths.
-            This is a dictionary which holds monster names (strings) as the keys and health amounts (int) as values. 
-            Initialize it with 5 different monster health pairs, NOT using .Add()
+             
 
-            Adding a new Monster
-            You need to add a new monster. 
-            Be sure to check if the monster exists in the dictionary already! You cannot add monsters that already exist. 
-            If it already exists, write to the console: "Tried to add a monster that already exists!" 
+            
 
             Get the HP of a monster and display it
             Declare an integer variable and store the HP of any monster in the dictionary in it. 
@@ -41,6 +33,39 @@ namespace CharlieDobson_MiniGame_Dictionary_Programming2
                 {"Barbarian", 150 }
             };
 
+          while(true)
+            {
+                Console.WriteLine("Insert a new monster name: ");
+                string newMonsterName = Console.ReadLine();
+
+                if(monsterHealth.ContainsKey(newMonsterName))
+                {
+                    Console.WriteLine("CANNOT ADD NEW MONSTER! MONSTER EXISTS!");
+                }
+                else
+                {
+                    Console.WriteLine("Add Monster Health");
+                    string newMonsterHealth = Console.ReadLine();
+
+                    if (int.TryParse(newMonsterHealth, out int result))
+                    {
+                        Console.WriteLine($"New monster added! {newMonsterName} with {result} health");
+                        monsterHealth.Add(newMonsterName, result);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("NOT A NUMBER!");
+                    }
+                }
+            }
+            Console.ReadKey(true);
+            Console.Clear();
+
+            foreach (var kvp in monsterHealth)
+            {
+                Console.WriteLine($"Monster name: {kvp.Key} with {kvp.Value} health.");
+            }
 
 
         }
